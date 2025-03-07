46d2ee75100dc711a607af235ce3c4501d3ac440
EssenceMaks <makski.wish@gmail.com>
2025-03-07 17:40:50 +0200

    vercel bug fix 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactSWC from '@vitejs/plugin-react-swc'; // Заменено с plugin-react на plugin-react-swc
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactSWC()], // Используем установленный плагин
+  base: '/',
+});
